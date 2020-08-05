078d8a74360c79cd08e8b5c1a0f9304c931cff4c
Rana Emad <rana3emad@gmail.com>
2020-08-05 10:50:04 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
 import "./App.css";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 
-function App() {
-  return <div className="app"></div>;
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <div className="app">
+          <Switch>
+            <Route path="/" exact />
+            <Route path="/signup" />
+            <Route path="/signin" />
+            <Route path="/dashboard" />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
